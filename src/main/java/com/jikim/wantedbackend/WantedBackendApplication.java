245e6eb2058d54082f1ff2fb9f5656bfea25d447
kho903 <gmldnr2222@naver.com>
2023-10-05 20:04:28 +0900

    conf: start wanted-backend project

---
+++ src/main/java/com/jikim/wantedbackend/WantedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jikim.wantedbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jikim/wantedbackend/WantedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jikim.wantedbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
