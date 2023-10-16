f737142a1007fe2212c4accb8f95d4d570d6e624
kho903 <gmldnr2222@naver.com>
2023-10-16 19:59:06 +0900

    feat: add RecruitmentRepository (JPA)

---
+++ src/main/java/com/jikim/wantedbackend/recruitment/RecruitmentRepository.java
@@ -0,0 +1,6 @@
+package com.jikim.wantedbackend.recruitment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+}
